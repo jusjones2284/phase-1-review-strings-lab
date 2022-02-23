4cdde186e1e842363ced615e3b1bfa32042b899c
Justin Jones <jus.jones@yahoo.com>
2022-02-23 16:36:22 -0500

    commit tree 6e5395cbe563503f28c9a75048dc203769aefe1b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'; 
+
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
+
+
+const myString = 'template literal';
+const myNumber = 10;
+const myBoolean = false;
+
+console.log(`Saying that interpolation with ${myString}s is better \n than concatenation ${90 + myNumber}% of the time ${myBoolean}. But it is pretty cool!`
+)
+
+
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
+function isRose(cat){
+    if(cat === 'rose'){
+        return 'Hi Rose you are cute!'
+    } else{
+        return `Hi ${cat}!`
+
+    }
+}
